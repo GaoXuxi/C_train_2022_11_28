9ab7d2e4f5755d20eb4f2289f14c38ce343a2d82
GaoXuxi <640340245@qq.com>
2022-11-28 23:13:27 +0800

    这是第一个题目

---
+++ 训练营_11_28.c
@@ -0,0 +1,11 @@
+//C语言  .c  头文件    .h  源文件
+
+//包含/引入头文件
+#include<stdio.h>
+
+int main()   //主函数
+{  //main函数是程序的入口，代码从main函数的第一行开始执行。一个工程中，有且仅有一个main函数
+	//printf  - 库函数  - 输出操作  - 得引入头文件   - stdio.h
+	printf("Practice makes perfect!");  //内容建议CV,确保无误！！！
+	return 0;
+}
